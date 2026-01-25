5dcb24d2185219bd87766e41001e2fb69361bbec
Sunny Kumar <sunnykumar49617@gmail.com>
2026-01-25 07:39:19 +0530

    backend setup done with documentations

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,4 +1,50 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Put,
+  Param,
+  Delete,
+  ParseIntPipe,
+  UsePipes,
+  ValidationPipe
+} from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
 
 @Controller('users')
-export class UsersController {}
+@UsePipes(new ValidationPipe({ whitelist: true, transform: true }))
+export class UsersController {
+  constructor(private readonly usersService: UsersService) { }
+
+  @Post()
+  create(@Body() createUserDto: CreateUserDto): User {
+    return this.usersService.create(createUserDto);
+  }
+
+  @Get()
+  findAll(): User[] {
+    return this.usersService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id: number): User {
+    return this.usersService.findOne(id);
+  }
+
+  @Put(':id')
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateUserDto: UpdateUserDto
+  ): User {
+    return this.usersService.update(id, updateUserDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id', ParseIntPipe) id: number): User {
+    return this.usersService.remove(id);
+  }
+}
--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,4 +1,54 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private users: User[] = [];
+  private idCounter = 1;
+
+  create(createUserDto: CreateUserDto): User {
+    const newUser: User = {
+      id: this.idCounter++,
+      ...createUserDto,
+    };
+    this.users.push(newUser);
+    return newUser;
+  }
+
+  findAll(): User[] {
+    return this.users;
+  }
+
+  findOne(id: number): User {
+    const user = this.users.find((user) => user.id === id);
+    if (!user) {
+      throw new NotFoundException(`User with ID ${id} not found`);
+    }
+    return user;
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto): User {
+    const user = this.findOne(id); // Throws if not found
+
+    if (updateUserDto.name) {
+      user.name = updateUserDto.name;
+    }
+    if (updateUserDto.email) {
+      user.email = updateUserDto.email;
+    }
+
+    // Ideally we would return a copy, but mutating in-memory object logic:
+    return user;
+  }
+
+  remove(id: number): User {
+    const index = this.users.findIndex((user) => user.id === id);
+    if (index === -1) {
+      throw new NotFoundException(`User with ID ${id} not found`);
+    }
+    const [deletedUser] = this.users.splice(index, 1);
+    return deletedUser;
+  }
+}
